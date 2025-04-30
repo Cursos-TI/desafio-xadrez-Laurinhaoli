68f072bae966239fadb7934f1ebb420b2d30f44e
Laurinha <laurinhaaholiveira@hotmail.com>
2025-04-30 04:19:41 +0000

    BISPO- for

---
+++ xadrezinicio.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+
+int main () {
+    int i = 8 
+
+    //mover o bispo 5 casas para a direita 
+
+    for (int i = 0; i < 5 ; i++) {
+    printf ("direita\n") ; // imprimir a direcao do movimento
+    
+    }
+  
+}        
+
+  return 0;
